6e7da58a62834c0b5da122a8329b6f12b3e4ad2e
Louise Grandjonc <louve.grandjonc@gmail.com>
2018-02-12 12:43:18 +0100

    update requirements

--- homeochoice/settings.py
+++ homeochoice/settings.py
@@ -130,3 +130,21 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
+# Heroku settings
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
